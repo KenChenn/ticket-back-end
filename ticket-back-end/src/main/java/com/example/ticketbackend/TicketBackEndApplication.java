da6650928f2203c98a0e5fcf763d0e467e5d1a7c
KenChenn <ar8410236@gmail.com>
2024-01-03 16:06:00 +0800

    first

---
+++ ticket-back-end/src/main/java/com/example/ticketbackend/TicketBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBackEndApplication.class, args);
+	}
+
+}
