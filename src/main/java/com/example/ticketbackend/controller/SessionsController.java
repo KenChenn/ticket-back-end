d4df58c7b506e6df629450514dcc2aaedf743027
KenChenn <ar8410236@gmail.com>
2024-01-08 14:03:35 +0800

    update

---
+++ src/main/java/com/example/ticketbackend/controller/SessionsController.java
@@ -0,0 +1,14 @@
+package com.example.ticketbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ticketbackend.service.ifs.SessionsService;
+
+@CrossOrigin
+@RestController
+public class SessionsController {
+
+
+}
